56cb5ff375aa5399d331d6040aa71208874a08e5
Subham Mahato <sbmmahato@gmail.com>
2024-03-15 22:18:26 +0530

    .

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
     next();
 
     app.use(cors({
-        origin:"https://hello-chat-silk.vercel.app",
+        origin:"*",
         credentials:true,
         methods:["GET","POST","PUT","DELETE"]
     }));
