d0c16aace5c55c5815c683bf60379b11dac64a0a
Subham Mahato <sbmmahato@gmail.com>
2024-03-15 22:14:55 +0530

    .

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 // }))
 
 
-app.all((req, res, next) => {
+app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Credentials', true);
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
